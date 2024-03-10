ab961f95cb79891f55de50148d05e6b35e0ba350
Melvyn <melvyn.vogelsang@he-arc.ch>
2024-03-10 21:58:27 +0100

    add debug

--- main.py
+++ main.py
@@ -176,10 +176,10 @@ class FeatureExtraction:
         try:
             url = self.whois_response.domain
             hostname = url
-            print(url)
             context = ssl.create_default_context()
             with socket.create_connection((hostname, 443)) as sock:
                 with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+                    print(ssock.getpeercert())
                     if 'issuer' in ssock.getpeercert():
                         value = 1
                         return {"feature": "HTTPS", "value": value, "reason": "Certificat HTTPS valide"}
